694cc212fd4cd9637443b8df823d19c9b4bf2c8f
swati <swati@gmail.com>
2022-03-05 09:46:40 +0530

    swati's first question

---
+++ src/main/java/myExamSDM/Exam.java
@@ -0,0 +1,10 @@
+package myExamSDM;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello this is swati..!");
+	}
+
+}
